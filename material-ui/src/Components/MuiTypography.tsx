b8035701c5cde917200c8500a6b4c5693e4b3844
geniusvinny <vkadenge89@gmail.com>
2023-11-09 18:58:16 +0300

    First commit

---
+++ material-ui/src/Components/MuiTypography.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Typography } from '@mui/material'
+
+const MuiTypography = () => {
+  return (
+    <div>
+        {/* varian ranges from H1 to H6 */}
+      <Typography variant='h1'>H1 heading</Typography>
+      <Typography variant='h2'>H2 heading</Typography>
+      <Typography variant='h3'>H3 heading</Typography>
+      <Typography variant='h4'>H4 heading</Typography>
+      <Typography variant='h5'>H5 heading</Typography>
+      <Typography variant='h6'>H6 heading</Typography>
+    </div>
+  )
+}
+
+export default MuiTypography
---
+++ material-ui/src/app/page.tsx
@@ -0,0 +1,10 @@
+import MuiTypography from '@/Components/MuiTypography'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center p-10">
+      <MuiTypography/>
+    </main>
+  )
+}
